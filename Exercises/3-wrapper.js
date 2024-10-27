c718f87ab735915fa654b03f68ec8394a57d9191
Fererra <126345645+Fererra@users.noreply.github.com>
2024-10-27 08:20:39 +0200

    completed task

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    const type = types[i];
+    const name = type.name.toLowerCase();
+    if (typeof arg !== name) {
+      throw new TypeError(`Argument type ${name} expected`);
+    }
+  }
+  const result = fn(...args);
+  const resultType = types[types.length - 1];
+  const name = resultType.name.toLowerCase();
+  if (typeof result !== name) {
+    throw new TypeError(`Result type ${name} expected`);
+  }
+  return result;
+};
 
 module.exports = { contract };
